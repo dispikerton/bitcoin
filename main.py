3e20fd517ab80d2db0045591da17c2f329d6c94c
jarvis <dmitrii.zolotenkov@diginetica.com>
2024-05-11 22:17:11 +0700

    Возня с зависимостями

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+from fastapi import FastAPI
+
 from data_preprocessing import load_data, prepare_data
 from model import create_model, train_model, evaluate_model
+
+app = FastAPI()
 
 file_path = 'bitcoin_2017_to_2023.csv'
 window_size = 60
 
 
-def main():
+@app.on_event("startup")
+async def startup_event():
+    global model, scaler
     data = load_data(file_path)
     print("Data loaded.")
     x_train, y_train, x_test, y_test, scaler = prepare_data(data, window_size)
@@ -18,5 +24,6 @@ def main():
     print("Model evaluated.")
 
 
-if __name__ == '__main__':
-    main()
+@app.get("/")
+async def root():
+    return {"message": "Bitcoin Price Prediction API"}
