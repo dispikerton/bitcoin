38198faf31e7b8aa5b74679ee3e7990a1fe68772
jarvis <dmitrii.zolotenkov@diginetica.com>
2024-05-11 22:41:06 +0700

    Поправляю импорты

--- model.py
+++ model.py
@@ -1,11 +1,18 @@
-from keras.models import Sequential
-from keras.layers import LSTM, Dense
+import tensorflow as tf
+from tensorflow import keras
+
+# Настройка TensorFlow для использования GPU
+physical_devices = tf.config.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+else:
+    print("No GPU found. Using CPU.")
 
 
 def create_model(window_size, num_features):
-    model = Sequential()
-    model.add(LSTM(50, input_shape=(window_size, num_features)))
-    model.add(Dense(1))
+    model = keras.Sequential()
+    model.add(keras.layers.LSTM(50, input_shape=(window_size, num_features)))
+    model.add(keras.layers.Dense(1))
     model.compile(optimizer='adam', loss='mse')
     return model
 
